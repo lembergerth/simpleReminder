1eac14573d5339aad761734377281ee5b012de5f
Leo Strakosch <shirmomi@gmail.com>
2014-06-30 21:30:26 +0200

    Corpus for SimpleGUI UI

---
+++ features/SimpleGUI/de/leostrakosch/reminder/ui/UI.java
@@ -0,0 +1,21 @@
+package de.leostrakosch.reminder.ui;
+
+import java.util.Observer;
+import java.util.Observable;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+public class UI extends JFrame implements Observer {
+
+  public UI() {
+
+  }
+
+  @Override
+  public void update(Observable observable, Object obj) {
+
+  }
+}
