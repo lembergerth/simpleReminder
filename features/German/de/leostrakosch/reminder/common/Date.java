f9a0f0f7e5422849a161a1ca15e66242a7b2488d
Tantalos <maximiliansyri@online.de>
2014-07-01 20:21:05 +0200

    Feature Date 'German format' finished

---
+++ features/German/de/leostrakosch/reminder/common/Date.java
@@ -0,0 +1,22 @@
+package de.leostrakosch.reminder.common;
+
+import java.util.StringTokenizer;
+
+/**
+ * TODO description
+ */
+public class Date {
+	private final String DELIM = ".";
+
+	public void interpretDateRepresentation(StringTokenizer st)
+			throws IllegalArgumentException {
+		day = Byte.parseByte(st.nextToken());
+		month = Byte.parseByte(st.nextToken());
+		year = Short.parseShort(st.nextToken());
+	}
+	
+	public String getStringRepresentation() {
+		return String.valueOf("" + day + DELIM + month + DELIM + year);
+	}
+
+}
