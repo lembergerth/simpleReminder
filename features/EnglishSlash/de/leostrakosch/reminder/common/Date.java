a272ac0c13e09403eedaf85f9d6f8ca4181670b3
Tantalos <maximiliansyri@online.de>
2014-07-01 20:20:31 +0200

    Feature Date Format English Slash finished

---
+++ features/EnglishSlash/de/leostrakosch/reminder/common/Date.java
@@ -0,0 +1,22 @@
+package de.leostrakosch.reminder.common;
+
+import java.util.StringTokenizer;
+
+/**
+ * TODO description
+ */
+public class Date {
+	private final String DELIM = "/";
+
+	public void interpretDateRepresentation(StringTokenizer st)
+			throws IllegalArgumentException {
+		
+		month = Byte.parseByte(st.nextToken());
+		day = Byte.parseByte(st.nextToken());
+		year = Short.parseShort(st.nextToken());
+	}
+	
+	public String getStringRepresentation() {
+		return String.valueOf("" + month + DELIM + day + DELIM + year);
+	}
+}
