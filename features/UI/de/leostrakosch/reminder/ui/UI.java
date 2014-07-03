da3fdf314e000e03743911f58a8fa13247945a61
Leo Strakosch <shirmomi@gmail.com>
2014-07-03 16:24:41 +0200

    Added stub for class UI in feature UI to resolve problems with MAYBE dependencies

---
+++ features/UI/de/leostrakosch/reminder/ui/UI.java
@@ -0,0 +1,14 @@
+package de.leostrakosch.reminder.ui;
+
+import java.util.Observable;
+
+/**
+ * Stub for GUI implementations of a feature
+ */
+public class UI implements Observer {
+
+  @Override
+  public void update(Observable observable, Object obj) {
+    throw new AssertionError("Unsupported update received");
+  }
+}
