17dfae183be6bea33dff39354e5596f4851cf8fd
Leo Strakosch <shirmomi@gmail.com>
2014-07-03 10:41:41 +0200

    TaskPanel displays Dates, if feature selected

---
+++ features/Dates/de/leostrakosch/reminder/ui/TaskPanel.java
@@ -0,0 +1,25 @@
+package de.leostrakosch.reminder.ui;
+
+import javax.swing.JPanel;
+
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+
+public class TaskPanel extends JPanel {
+
+  private FormLayout createLayout() {
+    return original();
+  }
+  
+  private void addComponents(Task t) {
+    original(t);
+    
+    CellConstraints cc = new CellConstraints();
+    FormLayout currLayout = (FormLayout) getLayout();
+    
+    currLayout.insertColumn(1, ColumnSpec.decode("fill:pref"));
+    currLayout.insertColumn(2, ColumnSpec.decode("4dlu"));
+    
+    add(new JLabel(t.getDueDate().getStringRepresentation()), cc.xy(1, 1));
+  }
+}
