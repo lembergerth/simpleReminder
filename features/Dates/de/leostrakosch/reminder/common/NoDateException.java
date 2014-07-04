b0ae1f2b426008fcd18443e13b41d61034018bd9
Tantalos <maximiliansyri@online.de>
2014-07-04 13:12:47 +0200

    Added a cach for tasks.
    getTasks created new instances of getTasks so that the attribute 'noted' were overwritten and triggering the alarm all the time

---
+++ features/Dates/de/leostrakosch/reminder/common/NoDateException.java
@@ -0,0 +1,10 @@
+package de.leostrakosch.reminder.common;
+
+/**
+ * TODO description
+ */
+public class NoDateException extends Exception {
+	public NoDateException(String error) {
+		super(error);
+	}
+}
--- features/Dates/de/leostrakosch/reminder/format/SeparatorFormatter.java
+++ features/Dates/de/leostrakosch/reminder/format/SeparatorFormatter.java
@@ -1,15 +1,18 @@
 package de.leostrakosch.reminder.format;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import de.leostrakosch.reminder.common.Configuration;
 import de.leostrakosch.reminder.common.IllegalLineFormatException;
 import de.leostrakosch.reminder.common.Task;
 import de.leostrakosch.reminder.common.Date;
+
 import java.util.Iterator;
 
 public class SeparatorFormatter {
+	private List taskCache = new ArrayList();
 
 	@Override
 	public String getString(Task t) {
@@ -20,29 +23,53 @@ public class SeparatorFormatter {
 
 	@Override
 	public Task getTask(String s) throws IllegalLineFormatException {
-    Task task;
-    Date dueDate;
+		Task task;
+		Date dueDate;
 		List attributes = splitInput(s, separator);
 		String currString;
 		String originalString = "";
-		String newAttribute = (String) attributes.get(attributes.size() - 1); // get last attribute
-		
-    for (Iterator it = attributes.iterator(); it.hasNext(); ) {
-      currString = (String) it.next();
-      
-      if (!it.hasNext()) {
-        continue; // don't take the last argument
-      }
-      
-      originalString += currString + separator;
-    }
-		
+		String newAttribute = (String) attributes.get(attributes.size() - 1); // get
+																				// last
+																				// attribute
+
+		for (Iterator it = attributes.iterator(); it.hasNext();) {
+			currString = (String) it.next();
+
+			if (!it.hasNext()) {
+				continue; // don't take the last argument
+			}
+
+			originalString += currString + separator;
+		}
+
 		task = original(originalString);
-		
+
 		dueDate = Date.valueOf(newAttribute);
 
 		task.setDate(dueDate);
-		
+
+		if (isTaskInCache(task) != null) {
+			return isTaskInCache(task);
+		}
+
+		// Caching tasks
+		taskCache.add(task);
+
 		return task;
+	}
+
+	private Task isTaskInCache(Task task) {
+		long id = task.getTaskID();
+		String name = task.getName();
+		Date date = task.getDueDate();
+		for (Object o : taskCache) {
+			Task cachedTask = (Task) o;
+			if (cachedTask.getTaskID() == id
+					&& cachedTask.getName().equals(name)
+					&& cachedTask.getDueDate().compareTo(date) == 0) {
+				return cachedTask;
+			}
+		}
+		return null;
 	}
 }
