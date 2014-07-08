65036b18ac9f04c2f6f094873a9102b90b61b929
Tantalos <maximiliansyri@online.de>
2014-07-08 18:48:52 +0200

    Added a check for a given string in Date. Checks if the string is a valid date representation

--- features/Dates/de/leostrakosch/reminder/common/Date.java
+++ features/Dates/de/leostrakosch/reminder/common/Date.java
@@ -5,34 +5,73 @@ import java.text.ParseException;
 import java.util.StringTokenizer;
 
 import de.leostrakosch.reminder.common.FeatureRuntimeError;
+import de.leostrakosch.reminder.common.NoDateException;
 
 /**
- * TODO description
- */
+*
+*/
 public class Date implements Serializable {
 
-  private static final long serialVersionUID = 2623834575450619003L;
-
+	private static final long serialVersionUID = 2623834575450619003L;
 
 	private final String DELIM = "";
 	private byte day;
 	private byte month;
 	private short year;
 
-	public Date(String dateRepresentation) throws IllegalArgumentException {
+	public Date(String dateRepresentation) throws NoDateException {
 		StringTokenizer st = new StringTokenizer(dateRepresentation, DELIM);
 		interpretDateRepresentation(st);
+
+		if (!(day > 0 && day < 32)) {
+			throw new NoDateException(day + " is no date in the calendar");
+		}
+		if (!(month > 0 && month < 13)) {
+			throw new NoDateException(month + " is no month in the calendar");
+		}
+		if (!(year > 0)) {
+			throw new NoDateException(year + " is no year");
+		}
+
+		// check if month has 31 days
+		{
+			switch (month) {
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				if (day == 31) {
+					throw new NoDateException("month " + month
+							+ "has not 31 days");
+				}
+				break;
+			case 2:
+				if (day == 29) {
+					if (year % 4 != 0) {
+						throw new NoDateException("");
+					}
+				}
+				if (day > 29) {
+					throw new NoDateException("");
+				}
+
+				break;
+			default:
+				throw new AssertionError("Unknown date: "
+						+ this.getStringRepresentation());
+			}
+		}
 	}
 
 	public void interpretDateRepresentation(StringTokenizer st)
-			throws IllegalArgumentException {
+			throws NoDateException {
 
 		throw new FeatureRuntimeError("date",
 				"Date format must be spezified by different features");
 	}
 
 	public static Date valueOf(String dateRepresentation)
-			throws IllegalArgumentException {
+			throws NoDateException {
 		return new Date(dateRepresentation);
 	}
 
